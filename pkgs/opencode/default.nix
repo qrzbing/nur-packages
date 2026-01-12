4198af127d76073e1a02cdccc34d7801fba22046
qrzbing <qrzbing@foxmail.com>
2026-01-12 09:41:38 +0800

    opencode: update to 1.1.13

--- pkgs/opencode/default.nix
+++ pkgs/opencode/default.nix
@@ -14,12 +14,12 @@
 }:
 let
   pname = "opencode";
-  version = "1.1.8";
+  version = "1.1.13";
   src = fetchFromGitHub {
     owner = "anomalyco";
     repo = "opencode";
     tag = "v${version}";
-    hash = "sha256-i0PYcI3qM52jcl72dqHxiBUQH5n4oBXTlxJqvWNS8CQ=";
+    hash = "sha256-prS39nJX5XpQCay5hC33UvGB4m6rOxVRtcrV77X9ejw=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
@@ -71,7 +71,7 @@ let
     # NOTE: Required else we get errors that our fixed-output derivation references store paths
     dontFixup = true;
 
-    outputHash = "sha256-NbKp1XXTirox/UnBSn/VfLtPXnjrDJxU94MaAh22wk4=";
+    outputHash = "sha256-vRIWQt02VljcoYG3mwJy8uCihSTB/OLypyw+vt8LuL8=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
