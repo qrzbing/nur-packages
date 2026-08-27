c8b114728ae43a56d0047829517abd7b98a2d2cd
qrzbing <qrzbing@foxmail.com>
2026-08-27 15:11:27 +0800

    general: migrate modules to nixosModules

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 # This file describes your repository contents.
 # It should return a set of nix derivations
-# and optionally the special attributes `lib`, `modules` and `overlays`.
+# and optionally the special attributes `lib`, `nixosModules` and `overlays`.
 # It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
@@ -11,9 +11,9 @@
 }:
 
 {
-  # The `lib`, `modules`, and `overlays` names are special
+  # The `lib`, `nixosModules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
+  nixosModules = import ./nixos-modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
   lanxin = pkgs.callPackage ./pkgs/lanxin { };
